d015ab1d341a41b68859ed991f38463e79c7cbab
Rui Nan <rui@stronghold.co>
2019-01-29 16:37:46 -0800

    save

--- programming/app.js
+++ programming/app.js
@@ -1,42 +1,60 @@
-const FindKthLargest = require('./findkthLargestElement');
-
-// Just a Math.random(), 
-// it requires the [1, 10], random() will give me [0, 1). 
+// Just a Math.random(),
+// it requires the [1, 10], random() will give me [0, 1).
 // So, it should times 10 and then plus 1
 
-function randomArraySize10 () {
+function randomArraySize10() {
     const result = [];
     for (let i = 0; i < 10; i++) {
-        result.push(Math.floor(Math.random() * 10 + 1));
+        result.push(Math.floor(Math.random() * 10 + 1)); // Generate a number belongs to the [1, 10], and push it into an array
     }
+    console.log('generated 10 random numbers', result);
     return result;
 }
 
-// There are two ways to find the 5th largest element in the array. 
 
+function findFifthLargestElement(nums) {
+    let k = 4; // because I use array[0] for comparasion.
+    const array = nums.slice(0); // clone to avoid mess up the original array
+    array.sort((a, b) => b - a); // sort high to low
+    let index = 0;
+    let i = 0;
+    for (i; i < array.length && k > 0; i++) { // avoid passing whole array to reduce the searching time.
+        if (array[i] !== array[index]) {
+            k--;
+            index = i;
+        }
+    }
+    return array[index];
+}
 
-function getIndicesOfLargestValues () {
+function getIndicesOfLargestValues() {
     let array = randomArraySize10();
+    const fifth = findFifthLargestElement(array);
     const result = [];
-    const fifth = FindKthLargest.findKthLargest2(array.slice(0), 5);
     for (let i = 0; i < array.length; i++) {
         if (array[i] >= fifth) {
             result.push(i);
         }
     }
+    console.log('Get the indices of largest values', result);
     return result;
 }
 
+// Problems: 
 // 1. JS does not have the type 'int'
 // 2. the callback function of setTimeout should be the async function. Because we call the async tasks sequentially.
 // 3. Depend on which JS version, if it is the es6, we use i to make sure the i could be a scoped variable. That makes the value of i be set into the async function each time. If we use var to declare the i, the task id will be 10.
-function identifyError () {
-    for (let i = 0; i < 10; i++){
-        setTimeout(async function(){
+// 4. The for loop will instantly set 10 setTimeout. And everyone will execute after 1 second. So the tasks will be call at same time
+
+function identifyError() {
+    let time = 0; // set base time
+    for (let i = 0; i < 10; i++) { // Replace the 'int' to 'let'
+        time += 1000; // set the start running time
+        setTimeout(function() { // setTimeout cannot make sure the callback runs at the timestamp. It is not precise.
             // Running some asynchronous tasks here
             // ...
-            console.log("completed task id " + i);
-        }, 1000)
+            console.log('completed task id: ', i, ' time: ', Date.now()); // I added the Date.now() to show the time when callback is running. 
+        }, time);
     }
 }
 
--- programming/readme.md
+++ programming/readme.md
@@ -1,5 +1,26 @@
-# The JS programming
+# The JS program
 
-## Solution
+## Dependencies
 
-Please run the 'npm start' to check the results.
+The only dependency is the Heap.
+
+## How to run it
+
+Under the folder in the terminal, run `npm start`. The result will show in the terminal console.
+
+## Description
+
+JS provides the Math object with a function named random() that provides a random value in [0, 1).
+
+Times the random value with 10 and parse to Integer, then plus 1, we get a random number in [1, 10],
+
+Repeat 10 times, we got an array with 10 random intergers.
+
+
+For getting the fifth ranked element indices, I need to consider the tie. It means the sorting is not a good solution that will break the index for each element in the original array. So, I thought I should find a way to find the 'Fifth Largest' element in the array. 
+
+Basically, I sorted a copy of array and pass from high to low to find the fifth largest element. The sort time complexity is O(nlogn), the find fifth time complexity is O(n). Meanwhile, we can use the Max Heap to do the same thing.
+
+Then I got the fifth largest element. Pass the whole array again, I got the array with the indices that represents the element which is larger or equal to the fifth element. And it hold the sequence of the indices.
+
+For the last one, it looks like to test the knowledge about the scope in JS. And the setTimeout in the JS. The first solution is to use let to replace the var. The i should change each time with i. Or it will always be 10. Second, the setTimeout will makes the all the callbacks run after 1 second. Because the for loop will excute instantly. It will send 10 setTimeout 
